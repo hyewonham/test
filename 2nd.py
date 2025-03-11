24a787c373845e910237e793390b74c84e5a377d
LG <hhw0099@naver.com>
2025-03-11 15:10:22 +0900

    first commit

---
+++ 2nd.py
@@ -0,0 +1,24 @@
+print("hello world")
+print(2+3)
+print("hello world!")
+print('hello', 'world')
+print('hello'+'world')
+print(100)
+print(2+4)
+r=5.0
+print('원의 반지름', r)
+print('원의 면적', 3.14 * r * r)
+print('원의 면적', 2.0 * 3.14 * r)
+name = '함혜원'
+print('이름 : ', name)
+width = 10
+height = 5
+rectangle_area = width * height
+print('사각형의 면적 :', rectangle_area)
+width = 20
+height = 5
+rectangle_area = width * height
+print('사각형의 면적 :', rectangle_area)
+Lst = [10, 20, 30]
+total = sum(Lst)
+print(total)
